f7cfe33af6cdbc82a4d32bb2885057368351a9e4
Rikelmy Lopes <rikelmylopes899@gmail.com>
2023-02-25 13:58:20 -0300

    implementado a logica dos contatos

--- frontend/src/Pages/Contacts.tsx
+++ frontend/src/Pages/Contacts.tsx
@@ -1,10 +1,37 @@
 import axios from 'axios';
-import React, { useCallback, useEffect} from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useEffect, useState} from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import Header from '../Components/Header';
 
 function Contacts() {
   const history = useNavigate();
+  const [name, setName] = useState<string>();
+  const [phoneNumber, setPhoneNumber] = useState<string>();
+  const [contacts, setContacts] = useState<any[]>([]);
+
+  const addContact = () => {
+    if (localStorage.getItem('contacts')) {
+      const contacts = JSON.parse(String(localStorage.getItem('contacts'))) as any[];
+      contacts.push({
+        name,
+        phoneNumber,
+      });
+      localStorage.setItem('contacts', JSON.stringify(contacts));
+      setContacts(contacts);
+    }
+    else {
+      localStorage.setItem('contacts', JSON.stringify([{ name, phoneNumber }]));
+      setContacts(JSON.parse(String(localStorage.getItem('contacts'))));
+    }
+  };
+
+  const retrieveContacts = () => {
+    if (localStorage.getItem('contacts')) {
+      setContacts(JSON.parse(String(localStorage.getItem('contacts'))));
+      return;
+    }
+    return;
+  };
 
   const validateToken = async (): Promise<void> => {
     const host = process.env.REACT_APP_BACKEND_HOST;
@@ -23,11 +50,39 @@ function Contacts() {
 
   useEffect(() => {
     validateToken();
+    retrieveContacts();
   }, []);
 
   return(
     <div>
       <Header/>
+      <input
+        placeholder='Nome'
+        type="text" 
+        name="" 
+        onChange={ ({ target }) => setName(target.value) }
+      />
+      <input
+        placeholder='Exemplo: +5538123456789'
+        type="text" 
+        name="" 
+        onChange={ ({ target }) => setPhoneNumber(target.value) }
+      />
+      <button
+        onClick={ addContact }
+      >
+          Adicionar Contato
+      </button>
+      <h3>Contatos</h3>
+      <div id='contacts-container'>
+        { contacts.map((contact) => (
+          <Link id='contacts'  key={`${contact.phoneNumber}`} to={`/chatWith/${contact.phoneNumber}`}>
+            <span> Nome: <strong>{ contact.name }</strong> </span>
+            <span> PhoneNumber: { contact.phoneNumber } </span>
+          </Link>
+        ))
+        }
+      </div>
     </div>
   );
 }
