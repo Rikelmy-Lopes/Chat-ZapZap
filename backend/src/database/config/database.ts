a280e5a14dd07303f1514c1da7d93cfc90fdcfe7
Rikelmy Lopes <rikelmylopes899@gmail.com>
2023-03-16 12:29:01 -0300

    corrigido conexão com banco de dados mysql

--- backend/src/database/config/database.ts
+++ backend/src/database/config/database.ts
@@ -3,7 +3,7 @@ import { Options } from 'sequelize';
 
 const config: Options = {
   username: process.env.DB_USER || 'root',
-  password: process.env.DB_PASS || '123456',
+  password: process.env.DB_PASS || '899899',
   database: 'chat-zap-zap',
   host: process.env.DB_HOST || 'localhost',
   port: Number(process.env.DB_PORT) || 3002,
