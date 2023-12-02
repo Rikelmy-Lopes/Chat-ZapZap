cb38d2960b33ea45f6103077337fa1dc5f9402a9
Rikelmy Lopes <rikelmylopes899@gmail.com>
2023-12-02 13:38:37 -0300

    feat: added env for port of the socket

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,14 +1,14 @@
-import app from './app';
 import 'dotenv/config';
+import app from './app';
 import connectToDatabase from './database/NOSQL/config/connection';
 import socket from './socket';
 
-
-const PORT = process.env.PORT || 3001;
+const API_PORT = process.env.API_PORT ? Number(process.env.API_PORT) : 3001;
+const SOCKET_PORT = process.env.SOCKET_PORT ? Number(process.env.SOCKET_PORT) : 4000;
 
 connectToDatabase().then(()=> {
-  socket.listen(4000, () => console.log(`Running server SocketIo on: http://localhost:${4000}`));
-  app.listen(Number(PORT), () => console.log(`Running server Express on: http://localhost:${PORT}`));
+  socket.listen(SOCKET_PORT, () => console.log(`Running server SocketIo on: http://localhost:${SOCKET_PORT}`));
+  app.listen(API_PORT, () => console.log(`Running server Express on: http://localhost:${API_PORT}`));
 
 }).catch((error) => {
   console.log('Connection with database generated an error:\r\n');
